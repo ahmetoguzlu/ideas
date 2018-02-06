487eafbdcb339d9be3920d1fc3e4688db8413bca
Ahmet Oguzlu <aoguzlu18@andover.edu>
2018-02-06 11:12:16 -0500

    Basic UI and Design

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,3 +3,6 @@
 
 ### Jan 30
 - A lot of updates on TODO.md and brainstorm for logistics. I also added some simple design ideas.
+
+### Feb 06
+- xCode design, worked on background basic UI etc
---
+++ Ideas/Ideas/DataViewController.swift
@@ -0,0 +1,34 @@
+//
+//  DataViewController.swift
+//  Ideas
+//
+//  Created by Ahmet Oğuzlu on 06/02/18.
+//  Copyright © 2018 Ahmet Oğuzlu. All rights reserved.
+//
+
+import UIKit
+
+class DataViewController: UIViewController {
+
+    @IBOutlet weak var dataLabel: UILabel!
+    var dataObject: String = ""
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        self.dataLabel!.text = dataObject
+    }
+
+
+}
+
